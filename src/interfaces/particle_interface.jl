0f9cad503beba9f23b01fa09e1b8c779b9165423
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-09-19 22:54:50 +0200

    update docstring of is_fermion and is_boson

--- src/interfaces/particle_interface.jl
+++ src/interfaces/particle_interface.jl
@@ -34,7 +34,7 @@ abstract type AbstractParticle end
 """
     $(TYPEDSIGNATURES)
 
-Interface function for particles. Return `true` if the passed subtype of `AbstractParticle` can be considered as a `fermion` in the sense of particle statistics, and `false` otherwise.
+Interface function for particles. Return `true` if the passed subtype of `AbstractParticle` can be considered as a *fermion* in the sense of particle statistics, and `false` otherwise.
 
 The default implementation of `is_fermion` for every subtype of `AbstractParticle` will always return `false`.
 """
@@ -43,7 +43,7 @@ Base.@pure is_fermion(::AbstractParticle) = false
 """
     $(TYPEDSIGNATURES)
 
-Interface function for particles. Return `true` if the passed subtype of `AbstractParticle` can be considered as a `boson` in the sense of particle statistics, and `false` otherwise.
+Interface function for particles. Return `true` if the passed subtype of `AbstractParticle` can be considered as a *boson* in the sense of particle statistics, and `false` otherwise.
 The default implementation of `is_boson` for every subtype of `AbstractParticle` will always return `false`.
 """
 Base.@pure is_boson(::AbstractParticle) = false
