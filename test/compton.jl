9d399949d28ae3b954022e1b2b09b7ab99a336bc
Anton Reinhard <s1509337@msx.tu-dresden.de>
2023-10-09 13:27:33 +0200

    Iterate through all pols and spins for all Compton tests

--- test/compton.jl
+++ test/compton.jl
@@ -2,65 +2,63 @@ using Random
 using QEDbase
 using QEDprocesses
 
-@testset "constructors" begin
-    @testset "default" begin
+POLS = [PolX(), PolY(), AllPol()]
+SPINS = [SpinUp(), SpinDown(), AllSpin()]
+POL_AND_SPIN_COMBINATIONS = Iterators.product(POLS, POLS, SPINS, SPINS)
+
+@testset "Default Compton constructor" begin
+    proc = Compton()
+    @test QEDprocesses._spin_or_pol(proc, Photon(), Incoming()) == AllPol()
+    @test QEDprocesses._spin_or_pol(proc, Electron(), Incoming()) == AllSpin()
+    @test QEDprocesses._spin_or_pol(proc, Photon(), Outgoing()) == AllPol()
+    @test QEDprocesses._spin_or_pol(proc, Electron(), Outgoing()) == AllSpin()
+end
+
+@testset "Compton with ($pol1, $pol2), ($spin1, $spin2)" for (pol1, pol2, spin1, spin2) in
+                                                             POL_AND_SPIN_COMBINATIONS
+    proc = nothing
+    @testset "constructor" begin
+        proc = Compton((pol1, pol2), (spin1, spin2))
+        @test QEDprocesses._spin_or_pol(proc, Photon(), Incoming()) == pol1
+        @test QEDprocesses._spin_or_pol(proc, Electron(), Incoming()) == spin1
+        @test QEDprocesses._spin_or_pol(proc, Photon(), Outgoing()) == pol2
+        @test QEDprocesses._spin_or_pol(proc, Electron(), Outgoing()) == spin2
+    end
+
+    @testset "scattering process interface" begin
+        @test incoming_particles(proc) == (Photon(), Electron())
+        @test outgoing_particles(proc) == (Photon(), Electron())
+        @test number_incoming_particles(proc) == 2
+        @test number_outgoing_particles(proc) == 2
+    end
+
+    @testset "invalid inputs" begin
+        model = PerturbativeQED()
         proc = Compton()
-        @test QEDprocesses._spin_or_pol(proc, Photon(), Incoming()) == AllPol()
-        @test QEDprocesses._spin_or_pol(proc, Electron(), Incoming()) == AllSpin()
-        @test QEDprocesses._spin_or_pol(proc, Photon(), Outgoing()) == AllPol()
-        @test QEDprocesses._spin_or_pol(proc, Electron(), Outgoing()) == AllSpin()
-    end
 
-    @testset "non-default" begin
-        pols = [PolX(), PolY(), AllPol()]
-        spins = [SpinUp(), SpinDown(), AllSpin()]
-        pol_and_spin_combinations = Iterators.product(pols, pols, spins, spins)
-        for (pol1, pol2, spin1, spin2) in pol_and_spin_combinations
-            proc = Compton((pol1, pol2), (spin1, spin2))
-            @test QEDprocesses._spin_or_pol(proc, Photon(), Incoming()) == pol1
-            @test QEDprocesses._spin_or_pol(proc, Electron(), Incoming()) == spin1
-            @test QEDprocesses._spin_or_pol(proc, Photon(), Outgoing()) == pol2
-            @test QEDprocesses._spin_or_pol(proc, Electron(), Outgoing()) == spin2
+        momenta_2 = [zero(SFourMomentum) for _ = 1:2]
+        momenta_3 = [zero(SFourMomentum) for _ = 1:3]
+
+        momenta_2_2 = Matrix{SFourMomentum}(undef, 2, 2)
+        momenta_3_2 = Matrix{SFourMomentum}(undef, 3, 2)
+
+        for (valid, invalid) in
+            Iterators.product([momenta_2, momenta_2_2], [momenta_3, momenta_3_2])
+            # try compute single input with incorrect dimensions
+            @test_throws DimensionMismatch differential_cross_section(
+                proc,
+                model,
+                invalid,
+                valid,
+            )
+            @test_throws "incoming" differential_cross_section(proc, model, invalid, valid)
+            @test_throws DimensionMismatch differential_cross_section(
+                proc,
+                model,
+                valid,
+                invalid,
+            )
+            @test_throws "outgoing" differential_cross_section(proc, model, valid, invalid)
         end
-    end
-end
-
-@testset "scattering process interface" begin
-    proc = Compton()
-
-    @test incoming_particles(proc) == (Photon(), Electron())
-    @test outgoing_particles(proc) == (Photon(), Electron())
-
-    @test number_incoming_particles(proc) == 2
-    @test number_outgoing_particles(proc) == 2
-end
-
-@testset "invalid inputs" begin
-    model = PerturbativeQED()
-    proc = Compton()
-
-    momenta_2 = [zero(SFourMomentum) for _ = 1:2]
-    momenta_3 = [zero(SFourMomentum) for _ = 1:3]
-
-    momenta_2_2 = Matrix{SFourMomentum}(undef, 2, 2)
-    momenta_3_2 = Matrix{SFourMomentum}(undef, 3, 2)
-
-    for (valid, invalid) in
-        Iterators.product([momenta_2, momenta_2_2], [momenta_3, momenta_3_2])
-        # try compute single input with incorrect dimensions
-        @test_throws DimensionMismatch differential_cross_section(
-            proc,
-            model,
-            invalid,
-            valid,
-        )
-        @test_throws "incoming" differential_cross_section(proc, model, invalid, valid)
-        @test_throws DimensionMismatch differential_cross_section(
-            proc,
-            model,
-            valid,
-            invalid,
-        )
-        @test_throws "outgoing" differential_cross_section(proc, model, valid, invalid)
     end
 end
