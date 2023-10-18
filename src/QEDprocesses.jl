e0c78fb2c23024312597d44b5b09dc3df1775ed9
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-10-18 23:39:41 +0200

    added dev-version of QEDbase and removed particle_types exports (accidentally added), temporarly solution

--- src/QEDprocesses.jl
+++ src/QEDprocesses.jl
@@ -13,12 +13,6 @@ export differential_cross_section, total_cross_section
 export AbstractComputationSetup, InvalidInputError, compute
 export AbstractProcessSetup, scattering_process, physical_model
 
-# particle types
-export AbstractParticleType
-export FermionLike, Fermion, AntiFermion, MajoranaFermion
-export BosonLike, Boson, AntiBoson, MajoranaBoson
-export Electron, Positron, Photon
-
 # propagator
 export propagator
 
@@ -29,6 +23,5 @@ include("utils.jl")
 include("interfaces/model_interface.jl")
 include("interfaces/process_interface.jl")
 include("interfaces/setup_interface.jl")
-include("particle_types.jl")
 include("propagators.jl")
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -15,9 +15,6 @@ begin
     end
 
     # modules
-    @time @safetestset "particles types" begin
-        include("particle_types.jl")
-    end
     @time @safetestset "propagators" begin
         include("propagators.jl")
     end
