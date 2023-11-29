a19b0fd87d0081e6ab0ebe3d918dc1a88050de0c
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-11-29 22:33:42 +0100

    added coordiante-based implementation for probabilities and differential cross sections

--- src/probabilities.jl
+++ src/probabilities.jl
@@ -236,6 +236,33 @@ function _differential_probability(
     in_phase_space_def::AbstractPhasespaceDefinition,
     in_phase_space::AbstractVector{T},
     out_phase_space_def::AbstractPhasespaceDefinition,
+    out_phase_space::AbstractVector{T},
+) where {T<:Real}
+
+    in_momenta, out_momenta = _generate_momenta(
+        proc,
+        model,
+        in_phase_space_def,
+        in_phase_space,
+        out_phase_space_def,
+        out_phase_space
+    )
+    return _differential_probability(
+        proc,
+        model,
+        in_phase_space_def,
+        in_momenta,
+        out_phase_space_def,
+       out_momenta 
+    )
+end
+    
+function _differential_probability(
+    proc::AbstractProcessDefinition,
+    model::AbstractModelDefinition,
+    in_phase_space_def::AbstractPhasespaceDefinition,
+    in_phase_space::AbstractVector{T},
+    out_phase_space_def::AbstractPhasespaceDefinition,
     out_phase_space::AbstractMatrix{T},
 ) where {T<:AbstractPhasespaceElement}
     res = Vector{eltype(T)}(undef, size(out_phase_space, 2))
