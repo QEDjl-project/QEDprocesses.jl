b1a9260d1128bf5e11b27590d45bb025e3802fa6
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-05-12 22:36:56 +0200

    added ylim finder

--- benchmark/plot_bench.jl
+++ benchmark/plot_bench.jl
@@ -4,6 +4,8 @@ Pkg.activate(".")
 using BenchmarkTools
 using BenchmarkPlots, StatsPlots
 
+include("utils.jl")
+
 plotpath = "plots"
 if !isdir(plotpath)
     mkdir(plotpath)
@@ -11,26 +13,32 @@ end
 
 result = BenchmarkTools.load("bench.json")[1]
 
-P = plot(result["stateful"]["accessor"]; yscale=:log10, ylim=(1e-1, 100))
+data = result["stateful"]["accessor"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: particle stateful accessor")
 savefig(joinpath(plotpath, "stateful_accessor.pdf"))
 
-P = plot(result["stateful"]["construction"]; yscale=:log10, ylim=(1, 1e10))
+data = result["stateful"]["construction"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: construction particle stateful")
 savefig(joinpath(plotpath, "stateful_construction.pdf"))
 
-P = plot(result["phase space point"]["generate"]; yscale=:log10, ylim=(1e2, 1e5))
+data = result["phase space point"]["generate"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: generate phase space points")
 savefig(joinpath(plotpath, "phase_space_generation.pdf"))
 
-P = plot(result["phase space point"]["construction"]; yscale=:log10, ylim=(1e1, 1e3))
+data = result["phase space point"]["construction"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: construction phase space points")
 savefig(joinpath(plotpath, "phase_space_construction.pdf"))
 
-P = plot(result["phase space point"]["momentum"]; yscale=:log10, ylim=(1e0, 1e3))
+data = result["phase space point"]["momentum"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: phase space points momentum access")
 savefig(joinpath(plotpath, "phase_space_momentum_access.pdf"))
 
-P = plot(result["phase space point"]["indexing"]; yscale=:log10, ylim=(1e0, 1e3))
+data = result["phase space point"]["indexing"]
+P = plot(data; yscale=:log10,ylim=_find_y_lims(data))
 plot!(P; title="benchmark: phase space points indexingaccess")
 savefig(joinpath(plotpath, "phase_space_momentum_indexing.pdf"))
