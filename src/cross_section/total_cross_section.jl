bbbfdea69cb333f03a0643d9e8df151856e9f7d7
Rubydragon <anton.reinhard@proton.me>
2024-06-27 14:46:13 +0200

    Fix docs building

--- src/cross_section/total_cross_section.jl
+++ src/cross_section/total_cross_section.jl
@@ -6,7 +6,7 @@
 """
     total_cross_section(in_psp::InPhaseSpacePoint)
 
-Return the total cross section for a given [`InPhaseSpacePoint`](@ref).
+Return the total cross section for a given `QEDcore.InPhaseSpacePoint`.
 """
 function total_cross_section(in_psp::InPhaseSpacePoint)
     I = 1 / (4 * QEDbase._incident_flux(in_psp))
--- src/cross_section/total_probability.jl
+++ src/cross_section/total_probability.jl
@@ -5,7 +5,7 @@
 """
     total_probability(in_psp::InPhaseSpacePoint)
 
-Return the total probability of a given [`InPhaseSpacePoint`](@ref).
+Return the total probability of a given `QEDcore.InPhaseSpacePoint`.
 """
 function total_probability(in_psp::InPhaseSpacePoint)
     return QEDbase._total_probability(in_psp)
