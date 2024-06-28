a5bdae56259e611c51e192947b615c2cae2f4508
Anton Reinhard <anton.reinhard@proton.me>
2024-06-28 21:41:34 +0200

    Remove abstract cs and probability implementations (#87)
    The implementations of the cross sections and probability functions for
    abstract processes have moved to QEDbase, see
    https://github.com/QEDjl-project/QEDbase.jl/pull/87. They are therefore
    now deleted here.
    
    Fixes issue https://github.com/QEDjl-project/QEDbase.jl/issues/86

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,13 +1,7 @@
-using QEDprocesses
 using Test
 using SafeTestsets
 
 begin
-    # modules
-    @time @safetestset "cross section & probability" begin
-        include("cross_sections.jl")
-    end
-
     # scattering processes
     include("processes/run_process_test.jl")
 end
