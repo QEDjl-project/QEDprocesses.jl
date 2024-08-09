a6a5a9fd0eeddc4bd34a744ab3057f0c4a488956
AntonReinhard <anton.reinhard@proton.me>
2024-08-09 14:26:32 +0200

    Use PackageExtensions, weak deps and conditional tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,5 @@ begin
     # scattering processes
     include("processes/run_process_test.jl")
 
-    @time @safetestset "GPU process interface" begin
-        include("gpu/process_interface.jl")
-    end
+    include("gpu/process_interface.jl")
 end
--- test/test_implementation/random_coordinates.jl
+++ test/test_implementation/random_coordinates.jl
@@ -6,3 +6,11 @@ function _rand_coordinates(
 ) where {PROCESS<:Compton,MODEL<:PerturbativeQED,PS_DEF<:PhasespaceDefinition}
     return ((rand(rng, Float64),), (rand(rng, Float64), rand(rng, Float64)))
 end
+
+tuple_isapprox(::Tuple{}, ::Tuple{}; atol=0.0, rtol=eps()) = true
+function tuple_isapprox(
+    a::Tuple{<:Number,Vararg}, b::Tuple{<:Number,Vararg}; atol=0.0, rtol=eps()
+)
+    return isapprox(a[1], b[1]; atol=atol, rtol=rtol) &&
+           tuple_isapprox(a[2:end], b[2:end]; atol=atol, rtol=rtol)
+end
