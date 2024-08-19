3f67b0fde56fe781e9f4be93234b248f8b3d8e6f
AntonReinhard <anton.reinhard@proton.me>
2024-08-19 17:09:03 +0200

    Add tests for generic process

--- src/processes/generic_process/process.jl
+++ src/processes/generic_process/process.jl
@@ -1,24 +1,71 @@
-mutable struct GenericQEDProcess{INT,OUTT,INSP,OUTSP} <: AbstractProcessDefinition where {
-    INT<:Tuple,OUTT<:Tuple,INSP<:Tuple,OUTSP<:Tuple
-}
+"""
+    GenericQEDProcess <: AbstractProcessDefinition
+
+An implementation of the `AbstractProcessDefinition` interface for a generic particle process in QED
+with any number of incoming and outgoing particles, and any combination of spins or polarizations for the particles set.
+
+The [`isphysical`](@ref) function can be used to check whether the process is possible in QED.
+
+!!! note
+    The computation of cross sections and probabilities is currently unimplemented.
+"""
+struct GenericQEDProcess{INT,OUTT,INSP,OUTSP} <:
+       AbstractProcessDefinition where {INT<:Tuple,OUTT<:Tuple,INSP<:Tuple,OUTSP<:Tuple}
     incoming_particles::INT
     outgoing_particles::OUTT
 
-    incoming_spins_pols::INSP
-    outgoing_spins_pols::OUTSP
+    incoming_spin_pols::INSP
+    outgoing_spin_pols::OUTSP
 
-    matrix_element_squared::Function
+    """
+        GenericQEDProcess(
+            in_particles::Tuple{AbstractParticleType},
+            out_particles::Tuple{AbstractParticleType},
+            in_sp::Tuple{AbstractSpinOrPolarization},
+            out_sp::Tuple{AbstractSpinOrPolarization}
+        )
 
+    Constructor for a GenericQEDProcess with the given incoming and outgoing particles and their respective spins and pols.
+    The constructor asserts that the particles are compatible with their respective spins and polarizations. If the assertion fails, an 
+    `InvalidInputError` is thrown.
+    """
     function GenericQEDProcess(
-        in_particles::INT, out_particles::OUTT, in_sp::INSP, out_sp::OUTSP
+        in_particles::INT, out_particles::OUTT, in_spin_pols::INSP, out_spin_pols::OUTSP
     ) where {INT<:Tuple,OUTT<:Tuple,INSP<:Tuple,OUTSP<:Tuple}
         _assert_particle_type_tuple(in_particles)
         _assert_particle_type_tuple(out_particles)
 
+        _assert_spin_pol_particle_compatability(in_particles, in_spin_pols)
+        _assert_spin_pol_particle_compatability(out_particles, out_spin_pols)
+
         return new{INT,OUTT,INSP,OUTSP}(
-            in_particles, out_particles, in_sp, out_sp, _ -> error("unimplemented")
+            in_particles, out_particles, in_spin_pols, out_spin_pols
         )
     end
+end
+
+"""
+    GenericQEDProcess(in_particles::Tuple{AbstractParticleType}, out_particles::Tuple{AbstractParticleType})
+
+Constructor for a GenericQEDProcess, setting `AllPol` and `AllSpin` for every boson and fermion, respectively.
+"""
+function GenericQEDProcess(
+    in_particles::INT, out_particles::OUTT
+) where {INT<:Tuple,OUTT<:Tuple}
+    # this will be called again by the default constructor, but it produces a nicer warning here
+    # than the following spin/pol generation failing because is_fermion or is_boson isn't defined on not allowed types
+    _assert_particle_type_tuple(in_particles)
+    _assert_particle_type_tuple(out_particles)
+
+    in_spin_pols = ntuple(
+        x -> is_fermion(in_particles[x]) ? AllSpin() : AllPolarization(),
+        length(in_particles),
+    )
+    out_spin_pols = ntuple(
+        x -> is_fermion(out_particles[x]) ? AllSpin() : AllPolarization(),
+        length(out_particles),
+    )
+    return GenericQEDProcess(in_particles, out_particles, in_spin_pols, out_spin_pols)
 end
 
 function QEDbase.incoming_particles(proc::GenericQEDProcess)
@@ -28,10 +75,10 @@ function QEDbase.outgoing_particles(proc::GenericQEDProcess)
     return proc.outgoing_particles
 end
 function QEDbase.incoming_spin_pols(proc::GenericQEDProcess)
-    return proc.incoming_spins_pols
+    return proc.incoming_spin_pols
 end
 function QEDbase.outgoing_spin_pols(proc::GenericQEDProcess)
-    return proc.outgoing_spins_pols
+    return proc.outgoing_spin_pols
 end
 
 """
@@ -46,4 +93,35 @@ function isphysical(proc::GenericQEDProcess)
         number_particles(proc, Incoming(), Positron()) +
         number_particles(proc, Outgoing(), Electron())
     ) && number_particles(proc, Incoming()) + number_particles(proc, Outgoing()) >= 2
+end
+
+function Base.show(io::IO, proc::GenericQEDProcess)
+    print(io, "generic QED process \"")
+    for p in incoming_particles(proc)
+        print(io, _particle_to_letter(p))
+    end
+    print(io, " -> ")
+    for p in outgoing_particles(proc)
+        print(io, _particle_to_letter(p))
+    end
+    print(io, "\"")
+    return nothing
+end
+
+function Base.show(io::IO, ::MIME"text/plain", proc::GenericQEDProcess)
+    println(io, "generic QED process")
+    for dir in (Incoming(), Outgoing())
+        first = true
+        for (p, sp) in zip(particles(proc, dir), spin_pols(proc, dir))
+            if !first
+                print(io, ", ")
+            else
+                print(io, "    $(dir): ")
+                first = false
+            end
+            print(io, "$(p) ($(sp))")
+        end
+        println(io)
+    end
+    return nothing
 end
--- src/processes/generic_process/utility.jl
+++ src/processes/generic_process/utility.jl
@@ -5,7 +5,41 @@ end
 function _assert_particle_type_tuple(t::Any)
     throw(
         InvalidInputError(
-            "invalid input, provide a tuple of AbstractParticleTypes to construct a QEDProcess",
+            "invalid input, provide a tuple of AbstractParticleTypes to construct a GenericQEDProcess",
         ),
     )
 end
+
+_assert_spin_pol_particle_compatability(::Tuple{}, ::Tuple{}) = nothing
+function _assert_spin_pol_particle_compatability(::Tuple{}, ::Tuple{Vararg})
+    throw(InvalidInputError("more spins/pols than particles given"))
+end
+function _assert_spin_pol_particle_compatability(::Tuple{Vararg}, ::Tuple{})
+    throw(InvalidInputError("more particles than spins/pols given"))
+end
+
+function _assert_spin_pol_particle_compatability(
+    particles::Tuple{AbstractParticleType,Vararg},
+    spin_pols::Tuple{AbstractSpinOrPolarization,Vararg},
+)
+    if is_fermion(particles[1]) && !(spin_pols[1] isa AbstractSpin)
+        throw(
+            InvalidInputError(
+                "particle \"$(particles[1])\" is a fermion and should have a spin, but has \"$(spin_pols[1])\"",
+            ),
+        )
+    end
+    if is_boson(particles[1]) && !(spin_pols[1] isa AbstractPolarization)
+        throw(
+            InvalidInputError(
+                "particle \"$(particles[1])\" is a boson and should have a polarization, but has \"$(spin_pols[1])\"",
+            ),
+        )
+    end
+    return _assert_spin_pol_particle_compatability(particles[2:end], spin_pols[2:end])
+end
+
+# this should move to QEDbase as part of the interface, see https://github.com/QEDjl-project/QEDbase.jl/issues/114
+_particle_to_letter(::Electron) = "e"
+_particle_to_letter(::Positron) = "p"
+_particle_to_letter(::Photon) = "k"
