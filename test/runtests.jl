b6ae7f56915602e2a72d5d9398a902f82e52e2e6
AntonReinhard <anton.reinhard@proton.me>
2024-09-08 14:03:53 +0200

    Review comments

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,7 @@ begin
     # scattering processes
     include("processes/run_process_test.jl")
 
-    include("gpu/process_interface.jl")
+    @time @safetestset "GPU tests" begin
+        include("gpu/process_interface.jl")
+    end
 end
